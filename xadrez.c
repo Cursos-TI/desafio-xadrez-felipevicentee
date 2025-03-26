a97e680a8d37b9250a298734fd842f1dc7ace458
felipevicentee <felipem905@gmail.com>
2025-03-26 00:57:50 +0000

    desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int movimentobispo = 1;
     int movimentorainha = 1;
-    int movimentocavalo, segundomovimento;
+    int movimentocavalo;
+    int segundomovimento;
     
     // mover a torre 
     printf("Movendo a torre:\n");
@@ -25,15 +27,16 @@ int main(){
         printf("Passo: %d Esquerda!\n", movimentorainha); //imprime a direção do movimento
         movimentorainha++;
     }
-     
+    
     //mover o cavalo
     printf("\nMovendo o Cavalo:\n");
     for (movimentocavalo = 1; movimentocavalo <= 2; movimentocavalo++) {
         printf("Passo: %d cima\n", movimentocavalo); //imprime a direção do movimento cima
     }    
-        for(segundomovimento = 1; segundomovimento <= 1; segundomovimento++) {
-        printf("Passo: %d direita\n", segundomovimento); //imprime a direção do movimento direta
-    }
+    do {
+        printf("Passo: %d direita!\n",movimentocavalo);  //imprime a direção do movimento direita
+                movimentocavalo++;
+            } while (movimentobispo <= 1);
 
     return 0;
 }
